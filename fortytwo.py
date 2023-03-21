c66b67fae4fe49c5af6cc96b00dfba35c1061bfa
Maurice Mertens <m.mertens@simplicity.ag>
2023-03-21 16:46:49 +0100

    feat: cool feature

--- fortytwo.py
+++ fortytwo.py
@@ -1,5 +1,10 @@
 def forty_two(number: int) -> bool:
     return number == 42
 
+
 def not_forty_two(number: int) -> bool:
     return number != 42
+
+
+def is_even(number: int) -> bool:
+    return number % 2 == 0
